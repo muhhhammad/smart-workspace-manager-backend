82a86a7bb3cc8df718738fcf4f71a3cae0c44ed4
Muhammmad Misbah <muhammad.misbah@paysyslabs.com>
2025-09-27 19:33:37 +0500

    Repository classes created.

--- src/main/java/com/fs/swm/repository/DepartmentRepository.java
+++ src/main/java/com/fs/swm/repository/DepartmentRepository.java
@@ -4,9 +4,11 @@ import com.fs.swm.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
-    Department  findByDepartmentId(Long id);
+    Optional<Department> findById(Long id);
 
 }
